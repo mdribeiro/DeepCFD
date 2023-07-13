a392836367446b3a81210bac75a6d54b1feadd6a
Shanthalakshmi Kilambi <s.kilambi@tu-braunschweig.de>
2023-07-13 13:36:13 +0200

    merge with GNN branch

--- applications/parametric_physicsinformedNN.py
+++ applications/parametric_physicsinformedNN.py
@@ -19,7 +19,7 @@ net = FeedForwardNN
 
 def parseOpts(argv):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    output = "Re_100_1000_wd_rs_model.pt"
+    output = "mymodel.pt"
     learning_rate = 0.001
     epochs = 500
     batch_size = 32
